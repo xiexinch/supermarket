7acfcee427d86bdfffd73aa62111ac4b93cb5ee2
TinkerAc <test767803@foxmail.com>
2016-02-14 21:50:22 +0800

    create 'Login'servlet

--- src/action/Login.java
+++ src/action/Login.java
@@ -7,35 +7,33 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Login
- */
+import com.supermarkt.Administrator;
+
 @WebServlet("/Login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public Login() {
         super();
-        // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
+		
+		
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		Administrator admin = new Administrator();
+		String username,password;
+		try{
+			username = request.getParameter("username");
+			password = request.getParameter("password");
+			admin.setName(username);
+			admin.setMypassword(password);
+			
+		}
+		catch (Exception ex){
+			ex.printStackTrace();
+		}
+		
 	}
 
 }
